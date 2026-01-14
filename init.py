38f10ff127c3e9acfc838f06a414b4a57ca65d76
apirrone <awanegenebayo@gmail.com>
2026-01-14 15:01:27 +0100

    init

---
+++ init.py
@@ -0,0 +1,55 @@
+from rustypot import Xl330PyController
+import time
+
+c = Xl330PyController("/dev/ttyACM0", baudrate=1000000, timeout=0.01)
+
+joints = {
+    "left_hip_yaw": 20,
+    "left_hip_roll": 21,
+    "left_hip_pitch": 22,
+    "left_knee": 23,
+    "left_ankle": 24,
+    "neck_pitch": 30,
+    "head_pitch": 31,
+    "head_yaw": 32,
+    "head_roll": 33,
+    "right_hip_yaw": 10,
+    "right_hip_roll": 11,
+    "right_hip_pitch": 12,
+    "right_knee": 13,
+    "right_ankle": 14,
+    }
+
+init_pose = [
+    0.0,
+    0.0,
+    0.75,
+    -1.5,
+    0.75,
+    
+    -0.5,
+    0.5,
+    0.0,
+    0.0,
+    
+    0.0,
+    0.0,
+    -0.9,
+    1.5,
+    -0.75,
+]
+    
+
+ids = [v for v in joints.values()]
+
+c.sync_write_torque_enable(ids, [True for _ in joints.keys()])
+c.sync_write_goal_position(ids, init_pose)
+# 
+# while True:
+    # 
+    # 
+    # print(c.sync_read_present_position(ids))
+    # 
+    # time.sleep(0.02)
+    # 
+    # 
